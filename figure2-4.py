5b1310091eebb89d264b167143f3a64d3fc8b425
Antoine Poirot-Bourdain <antoine.poirot.bourdain@gmail.com>
2026-06-12 00:02:15 +0200

    fixed figure2-4

--- figure2-4.py
+++ figure2-4.py
@@ -1,99 +1,94 @@
-from cox_functions import run_experiment
-import pandas as pd
+import sys
+import os
 import numpy as np
 from pathlib import Path
 
+lib_path = os.path.abspath("/home/dev/survivalGPU/python")
+if lib_path not in sys.path:
+    sys.path.insert(0, lib_path)
+
+from cox_functions import run_cox_experiment
 
 
 ########## Figure 2
 
-
 experiment_name = "figure_2"
-#_patients_list = [500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000]
-n_patients_list = [250,500]
+n_patients_list = [250, 500]
 n_constant_covariates = 1
 n_time_dependent_covariates = 0
-package_list = ["survivalgpu", "torchsurv", "xgboost"] # list of packages to benchmark
-device = "cuda"
 n_bootstraps = 0
 output_folder = Path("results/")
+n_iterations = 3
 
-package_device_list = [
-    ("survivalgpu", "cuda"),
-    ("survivalgpu", "cpu"),
-    ("survival", "cpu"),
-    ("lifelines", "cpu"),
+package_device_dtype_list = [
+    ("survivalgpu", "cuda", np.float32),
+    ("survivalgpu", "cpu", np.float32),
+    ("survival", "cpu", np.float64),
+    ("lifelines", "cpu", np.float64),
 ]
 
-
-experiment_result_2 = run_experiment(
-        experiment_name = experiment_name,
-        output_folder = output_folder,
-        n_patients_list = n_patients_list,
-        covariate_combination = [(n_constant_covariates, n_time_dependent_covariates)],
-        package_list = package_list,
-        n_bootstraps=n_bootstraps
-    )
-
+run_cox_experiment(
+    experiment_name=experiment_name,
+    output_folder=output_folder,
+    n_patients_list=n_patients_list,
+    covariate_combination=[(n_constant_covariates, n_time_dependent_covariates)],
+    package_device_dtype_list=package_device_dtype_list,
+    n_bootstraps=n_bootstraps,
+    n_iterations=n_iterations,
+)
 
 
 ########### Figure 3
 
-
 experiment_name = "figure_3"
-#_patients_list = [500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000]
-n_patients_list = [250,500]
+n_patients_list = [250, 500]
 n_constant_covariates = 1
 n_time_dependent_covariates = 0
 n_bootstraps = 1000
-output_folder = Path("/home/dev/analysis_survivalgpu/JSS/benchmarks/benchmark_results")
+output_folder = Path("results/")
+n_iterations = 3
 
-package_device_list = [
-    ("survivalgpu", "cuda"),
-    ("survivalgpu", "cpu"),
-    ("survival", "cpu"),
-    ("lifelines", "cpu"),
+package_device_dtype_list = [
+    ("survivalgpu", "cuda", np.float32),
+    ("survivalgpu", "cpu", np.float32),
+    ("survival", "cpu", np.float64),
+    ("lifelines", "cpu", np.float64),
 ]
 
-
-
-experiment_result_2 = run_experiment(
-        experiment_name = experiment_name,
-        output_folder = output_folder,
-        n_patients_list = n_patients_list,
-        covariate_combination = [(n_constant_covariates, n_time_dependent_covariates)],
-        package_device_list = package_device_list,
-        n_bootstraps=n_bootstraps
-    )
-
-
+run_cox_experiment(
+    experiment_name=experiment_name,
+    output_folder=output_folder,
+    n_patients_list=n_patients_list,
+    covariate_combination=[(n_constant_covariates, n_time_dependent_covariates)],
+    package_device_dtype_list=package_device_dtype_list,
+    n_bootstraps=n_bootstraps,
+    n_iterations=n_iterations,
+)
 
 
 ############ Figure 4
 
-
 experiment_name = "figure_4"
-#_patients_list = [500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000]
-n_patients_list = [250,500]
+n_patients_list = [250, 500]
 n_constant_covariates = 1
 n_time_dependent_covariates = 0
-package_list = ["survivalgpu", "torchsurv", "xgboost"] # list of packages to benchmark
 n_bootstraps = 0
-output_folder = Path("/home/dev/analysis_survivalgpu/JSS/benchmarks/benchmark_results")
+output_folder = Path("results/")
+n_iterations = 3
 
-package_device_list = [
-    ("survivalgpu", "cuda"),
-    ("survival", "cpu"),
-    ("xgboost", "cpu"),
-    ("torchsurv", "cpu"),
+package_device_dtype_list = [
+    ("survivalgpu", "cuda", np.float32),
+    ("survival", "cpu", np.float64),
+    ("xgboost", "cpu", np.float32),
+    ("torchsurv", "cpu", np.float32),
 ]
 
-
-experiment_result_2 = run_experiment(
-        experiment_name = experiment_name,
-        output_folder = output_folder,
-        n_patients_list = n_patients_list,
-        covariate_combination = [(n_constant_covariates, n_time_dependent_covariates)],
-        package_device_list = package_device_list,
-        n_bootstraps=n_bootstraps
-    )
+run_cox_experiment(
+    experiment_name=experiment_name,
+    output_folder=output_folder,
+    n_patients_list=n_patients_list,
+    covariate_combination=[(n_constant_covariates, n_time_dependent_covariates)],
+    package_device_dtype_list=package_device_dtype_list,
+    n_bootstraps=n_bootstraps,
+    n_iterations=n_iterations,
+)
